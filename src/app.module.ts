ad5b1dc77062a3cd224b0bf79cf04f44fc850465
Maique Rosa <maique.rosa@gmail.com>
2023-03-04 16:43:55 -0300

    chore: add enviroment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './modules/user/user.module';
-import { AccountModule } from './modules/account/account.module';
-import { CategoryModule } from './modules/category/category.module';
-import { RegisterModule } from './modules/register/register.module';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UserModule } from "./modules/user/user.module";
+import { AccountModule } from "./modules/account/account.module";
+import { CategoryModule } from "./modules/category/category.module";
+import { RegisterModule } from "./modules/register/register.module";
+import { ConfigModule } from "@nestjs/config";
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      'mongodb+srv://domzinhuu:g0e20KUq33urHtCU@cluster0.tpqyipj.mongodb.net/contatudo?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.tpqyipj.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`
     ),
     UserModule,
     AccountModule,
